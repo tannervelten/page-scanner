faf48cb196187bc159f6737ae824986c02796550
tannervelten <tannervelten@gmail.com>
2019-02-03 19:13:45 -0800

    init commit

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const puppeteer = require('puppeteer')
+const vision = require('@google-cloud/vision')
+const sgMail = require('@sendgrid/mail')
+const fs = require('fs')
+const AWS = require('aws-sdk')
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+const url = 'https://shop.dreamville.com/products/dreamville-hoodie-black?variant=8157889396826'
+let temp = 'not processed'
+
+function isSoldOut(text) {
+  return text.toLowerCase().includes('sold out')
+}
+
+async function quickstart() {
+  const client = new vision.ImageAnnotatorClient()
+
+  const [result] = await client.textDetection('resources/hoodie.png')
+  const soldOut = isSoldOut(result.fullTextAnnotation.text)
+  if (!soldOut) {
+    sendEmail()
+    temp = 'available'
+    return
+  }
+  console.log('Hoodie still sold out')
+  temp = 'Hoodie is still sold out'
+}
+
+const sendEmail = () => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const msg = {
+    to: 'tannervelten@gmail.com',
+    from: 'page-scanner@gmail.com',
+    subject: 'Dreamville hoodie available!',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+  sgMail.send(msg)
+}
+
+async function checkAvailability() {
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
+  const page = await browser.newPage()
+  await page.goto(url)
+  await page.screenshot({ path: 'resources/hoodie.png', fullPage: true })
+
+  try {
+    await quickstart()
+  } catch (err) {
+    console.log('err:', err)
+  }
+  await browser.close()
+}
+
+app.get('/', async (req, res) => {
+  await checkAvailability()
+  res.send(temp)
+})
+
+async function awsConfig() {
+  const fileKey = 'page-scanner-21b36b626096.json'
+  if (await !fs.existsSync(`${fileKey}`)) {
+
+    await AWS.config.update(
+      {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: 'us-east-1'
+      }
+    )
+    const s3 = new AWS.S3();
+    const options = {
+      Bucket: 'page-scanner-credentials',
+      Key: fileKey,
+    }
+
+    await s3.getObject(options).createReadStream().pipe(fs.createWriteStream(`${fileKey}`))
+  }
+}
+
+app.listen(PORT, async () => {
+  await awsConfig()
+  await checkAvailability()
+  console.log(`Listening on port ${PORT}`)
+})
